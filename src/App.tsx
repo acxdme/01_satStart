bb48edaed70392b419978aac13f4019fad55d351
acxdme <ayush.chaudhari8@gmail.com>
2024-01-07 21:16:47 +0530

    tailwind and svg changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,16 @@ import './App.css'
 import ActionAreaCard from './components/Card';
 import satelliteData from './service/SatelliteData';
 import { ISatellite } from './types/ISatelliteData';
+
 function App() {
-
-
   return (
     <>
-      <h1>Satellite Tracking System</h1>
-      {satelliteData?.map((satellite: ISatellite) => {
-        return (<ActionAreaCard name={satellite.name} purpose={satellite.purpose} />)
-      })}
+      <h1 className='text-3xl font-serif font-semibold p-4'>Satellite Tracking System</h1>
+      <div className='grid grid-cols-3 gap-4'>
+        {satelliteData?.map((satellite: ISatellite) => {
+          return (<ActionAreaCard name={satellite.name} purpose={satellite.purpose} />)
+        })}
+      </div>
     </>
   )
 }
