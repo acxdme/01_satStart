b69539deeb9633f69d4c86b3581909e1a585326d
acxdme <ayush.chaudhari8@gmail.com>
2024-01-21 10:37:52 +0530

    dataGrid component added

---
+++ src/components/DataGrid.tsx
@@ -0,0 +1,51 @@
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(
+    name: string,
+    calories: number,
+) {
+    return { name, calories };
+}
+
+const rows = [
+    createData('Frozen yoghurt', 159),
+    createData('Ice cream sandwich', 237),
+    createData('Eclair', 262),
+    createData('Cupcake', 305),
+    createData('Gingerbread', 356),
+];
+
+export default function DataTable() {
+    return (
+        <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Satellite Property</TableCell>
+                        <TableCell align="right">Value</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {rows.map((row) => (
+                        <TableRow
+                            key={row.name}
+                            sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                        >
+                            <TableCell component="th" scope="row">
+                                {row.name}
+                            </TableCell>
+                            <TableCell align="right">{row.calories}</TableCell>
+
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </TableContainer>
+    );
+}
--- src/components/Details.tsx
+++ src/components/Details.tsx
@@ -1,15 +1,22 @@
 import { useLocation } from "react-router-dom";
 import ButtonUsage from "./ButtonUsage";
+import "../styles/backButton.css";
+import DataTable from "./DataGrid";
 
 function Details() {
 
   const location = useLocation();
 
   return (
-    <div>
+    <>
       Satellite Name : {location?.state?.Name}
-      <ButtonUsage buttonText={"Back"} />
-    </div>
+      <div className={"back-button"}>
+        <ButtonUsage buttonText={"Back"} />
+      </div>
+      <div className={"data-grid"}>
+        <DataTable />
+      </div>
+    </>
   )
 }
 
